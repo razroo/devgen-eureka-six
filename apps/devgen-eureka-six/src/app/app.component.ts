df56791b7cbae2b6d8c9ae5156ea53ef59fc791f
Charlie Greenman <charlie@razroo.com>
2023-10-05 15:42:21 -0400

    Add code for angular-16.2.0 create-workspace.
    [Url: razroo.com/community/angular-16.2.0/create-workspace-recipe/create-workspace]

---
+++ apps/devgen-eureka-six/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'devgen-eureka-six-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'devgen-eureka-six';
+}
